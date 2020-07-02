a770c207491ce254f46d2d46438413a3a6be12a6
Monam94 <monam0750@gmail.com>
2020-07-02 16:21:09 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,30 @@
-/*****************************Javascript Code ****************************/
+
+//PreLoader //
+
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	
+	
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+    
+
+})(jQuery);
